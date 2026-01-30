e1ddbb4c809f7eda048bce372d6e91053d1e75ef
Hidha <hidhahidha03@gmail.com>
2026-01-30 15:58:57 +0530

    Time: 1 ms (32.21%), Space: 56.6 MB (17.98%) - LeetHub

---
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @param {number} m
+ * @return {number}
+ */
+var differenceOfSums = function(n, m) {
+    let divisible=0
+    let nondivisible=0
+    for(let i=1;i<=n;i++){
+        if(i%m==0){
+           divisible+=i
+        }else{
+            nondivisible+=i
+        }
+
+    }return nondivisible-divisible
+}; 
+
