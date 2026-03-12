2d6e06ecfd66714c004315bb5d92bd10f9042128
Hidha <hidhahidha03@gmail.com>
2026-03-12 13:57:19 +0530

    Time: 2 ms (35.23%), Space: 52.7 MB (100%) - LeetHub

--- 3019-number-of-changing-keys/3019-number-of-changing-keys.js
+++ 3019-number-of-changing-keys/3019-number-of-changing-keys.js
@@ -5,10 +5,10 @@
 var countKeyChanges = function(s) {
     let letters=s.toLowerCase().split('')
     let count=0
-    for(let i=0;i<letters.length-1;i++){
+    for(let i=0;i<letters.length;i++){
         if(letters[i]!==letters[i+1]){
             count++
         }
     }
-    return count
+    return count-1
 };
