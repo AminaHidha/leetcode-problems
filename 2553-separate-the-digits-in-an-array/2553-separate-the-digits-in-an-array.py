1d23bbd8869474d6159e69a610f66af8d5d59af0
Hidha <hidhahidha03@gmail.com>
2026-05-01 10:12:34 +0530

    Time: 11 ms (71.32%), Space: 12.5 MB (95.59%) - LeetHub

---
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def separateDigits(self, nums):
+        result=[]
+        for i in nums:
+            for j in str(i):
+                result.append(int(j))
+        return result
+        
