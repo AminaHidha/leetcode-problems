1e6c43299e804cef08badce35e5d2cc25f9813a3
Hidha <hidhahidha03@gmail.com>
2026-05-11 11:40:05 +0530

    Time: 0 ms (100%), Space: 12.4 MB (17.85%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def balancedStringSplit(self, s):
+        count=0
+        result=0
+        for i in range(len(s)):
+            if s[i]=="R":
+                count+=1
+            else:
+                count-=1
+            if count==0:
+                result+=1
+        return result
+        
+        
