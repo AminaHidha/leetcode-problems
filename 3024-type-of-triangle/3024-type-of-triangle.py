3ca9199278ac54f2e449ff34180a711b889111c5
Hidha <hidhahidha03@gmail.com>
2026-08-19 16:04:32 +0530

    Time: 0 ms (100%), Space: 12.2 MB (88.44%) - LeetHub

--- 3024-type-of-triangle/3024-type-of-triangle.py
+++ 3024-type-of-triangle/3024-type-of-triangle.py
@@ -1,8 +1,16 @@
 class Solution(object):
     def triangleType(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: str
+        """
         a, b, c = nums
+
+        # Check if the sides can form a triangle
         if a + b <= c or a + c <= b or b + c <= a:
             return "none"
+
+        # Check the type of triangle
         if a == b == c:
             return "equilateral"
         elif a == b or b == c or a == c:
