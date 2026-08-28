94b610100f2fafd798615f850f43d4816cd3f239
Hidha <hidhahidha03@gmail.com>
2026-08-28 10:03:46 +0530

    Time: 4 ms (54.18%), Space: 57.5 MB (47.6%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -1,24 +1,18 @@
 var majorityElement = function(nums) {
-    let count={}
-    for(i=0;i<nums.length;i++){
-        value=nums[i]
-        if(count[value]){
-            count[value]++
-        }
-        else{
-            count[value]=1
+    let candidate = null;
+    let count = 0;
+
+    for (let num of nums) {
+        if (count === 0) {
+            candidate = num;
         }
 
+        if (num === candidate) {
+            count++;
+        } else {
+            count--;
+        }
     }
-    console.log(count);
-    let maxcount=0
-    result=null
-    for (let key in count){
-       if(count[key]>maxcount){
-        maxcount=count[key]
-        result=key
-       }
-    }
-    return Number(result)
-    
+
+    return candidate;
 };
